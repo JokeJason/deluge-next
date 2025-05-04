a2cac197cc01cab9210c3a791ab178b7df2a5177
Jason Zhu <jasonzhuyq@outlook.com>
2025-05-04 12:11:32 +1000

    Add loading state to sign-out button for improved user feedback

--- src/components/sign-out-button.tsx
+++ src/components/sign-out-button.tsx
@@ -3,13 +3,24 @@
 import { signout } from '@/app/actions/auth';
 import { Button } from '@/components/ui/button';
 import 'client-only';
+import { Loader2 } from 'lucide-react';
+import { useState } from 'react';
 
 export default function SignOutButton() {
+  const [isSigningOut, setIsSigningOut] = useState(false);
+
   const handleSignOut = async () => {
+    setIsSigningOut(true);
+
     await signout();
   };
 
-  return (
+  return isSigningOut ? (
+    <Button disabled>
+      <Loader2 className={'animate-spin'} />
+      Signing out...
+    </Button>
+  ) : (
     <Button className={'hover:scale-110'} onClick={handleSignOut}>
       Sign out
     </Button>
